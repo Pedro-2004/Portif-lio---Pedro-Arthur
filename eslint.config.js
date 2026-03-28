c1c4f54b56df115780dd8dc26ca6211c938fcde0
Pedro Arthur <arthur.resende758@gmail.com>
2026-03-28 00:41:40 -0300

    Creation of component body

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist', 'node_modules'] },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+
+      // --- REGRAS DO PEDRO ---
+
+      // 1. Forçar Aspas Simples
+      quotes: ['error', 'single'],
+
+      // 2. Forçar Arrow Functions (Transforma function Name() em const Name = ())
+      'func-style': ['error', 'expression'],
+      'prefer-arrow-callback': 'error',
+
+      // 3. Ignorar erro de import com @ (Roteamento de pasta)
+      'import/no-unresolved': 'off',
+
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
-])
+];
---
+++ src/page/home/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+const Home = () => {
+  return (
+    <div className='home_container'>
+      <h1> Pedro Arthur Ferreira Resende</h1>
+      <h1> Desenvolvedor Full Stack</h1>
+      <h1> Hoje em dia eu trabalho na seteloc</h1>
+      <h1> olá </h1>
+    </div>
+  );
+};
+export default Home;
